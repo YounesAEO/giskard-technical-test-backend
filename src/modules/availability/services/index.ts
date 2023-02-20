9fb8f890a40f289675a712c7689f4636731239ca
YounesAEO <y.aboueleomoum09@gmail.com>
2023-02-20 17:01:40 +0100

    add createAvalability service to availability module

--- src/modules/availability/services/index.ts
+++ src/modules/availability/services/index.ts
@@ -1,1 +1,18 @@
-export * from './base';
+import Joi, { Schema } from 'joi';
+import * as BaseServices from './base';
+
+export const createAvailability = (data: any) => {
+	// validate data
+	const schema: Schema = Joi.object({
+		start: Joi.date().required(),
+		end: Joi.date().required(),
+		days: Joi.array().items(Joi.boolean()).length(7).required(),
+	});
+	const { error } = schema.validate(data);
+	if (error) {
+		throw new Error('Invalid data');
+	}
+
+	// create availability
+	return BaseServices.createOne({ payload: data });
+};
