2f334d02eaa6c69c6a687ae1c62511733d076fc7
YounesAEO <y.aboueleomoum09@gmail.com>
2023-02-21 10:39:02 +0100

    add deleteReservation service

--- src/modules/reservation/services/index.ts
+++ src/modules/reservation/services/index.ts
@@ -14,6 +14,25 @@ export const createReservation = (data: any) => {
 		throw new Error('Invalid details');
 	}
 
-	// create availability
+	// create reservation
 	return BaseService.createOne({ payload: data });
+};
+
+export const deleteReservation = async (data: any) => {
+	// validate data
+	const schema: Schema = Joi.object({
+		id: Joi.string().alphanum().length(24).required(),
+	});
+	const { error } = schema.validate(data);
+	if (error) {
+		throw new Error('Invalid details');
+	}
+
+	const reservation = await BaseService.deleteById({ id: data.id });
+
+	if (!reservation) {
+		throw new Error('No reservation found');
+	}
+
+	return reservation;
 };
