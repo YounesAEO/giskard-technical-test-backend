3a274de05288c02fb847f17c50ea2b2676698d22
YounesAEO <y.aboueleomoum09@gmail.com>
2023-02-21 02:27:42 +0100

    add jest.resetAllMocks after finishing test

--- src/modules/availability/tests/createAvailability/service.spec.ts
+++ src/modules/availability/tests/createAvailability/service.spec.ts
@@ -2,17 +2,16 @@ import { defineFeature, loadFeature } from 'jest-cucumber';
 import { jest } from '@jest/globals';
 import * as path from 'path';
 import { IAvailability } from '../../types';
-import * as AvailabilityService from '../../services';
-import * as BaseServices from '../../services/base';
+import { createAvailability } from '../../services';
+import * as BaseService from '../../services/base';
 
 const feature = loadFeature(path.join(__dirname, './use-case.feature'));
 defineFeature(feature, (test) => {
-	let start: Date;
-	let end: Date;
-	let availability: IAvailability;
-	let days: boolean[];
 	test('Create an availibility', ({ given, when, then }) => {
-		//@ts-ignore
+		let start: Date;
+		let end: Date;
+		let availability: IAvailability;
+		let days: boolean[];
 		given('I provide valid availibility details', () => {
 			//Arrange
 			start = new Date();
@@ -24,10 +23,10 @@ defineFeature(feature, (test) => {
 		when('I attempt to create an availibility', async () => {
 			//Act
 			// create a spy on the function that connects to the database
-			const createOneSpy = jest.spyOn(BaseServices, 'createOne');
+			const createOneSpy = jest.spyOn(BaseService, 'createOne');
 			createOneSpy.mockReturnValue(Promise.resolve({ start, end, days }));
 
-			availability = await AvailabilityService.createAvailability({
+			availability = await createAvailability({
 				start,
 				end,
 				days,
@@ -39,20 +38,58 @@ defineFeature(feature, (test) => {
 			expect(availability).toBeDefined();
 			expect(availability.start).toEqual(start);
 			expect(availability.end).toEqual(end);
+			expect(availability.days).toEqual(days);
+		});
+
+		afterEach(() => {
+			// reset the mock after each test
+			jest.resetAllMocks();
 		});
 	});
 
 	test('Invalid availibility details', ({ given, when, then }) => {
+		let start: Date;
+		let end: Date;
+		let availability: IAvailability;
+		let days: boolean[];
+		let err: Error;
 		given('I provide invalid start or end time', () => {
 			//Arrange
+			start = new Date('invalid');
+			// end date is 1 hour after start date
+			end = new Date(start.getTime() + 60 * 60 * 1000);
+			days = [];
 		});
 
-		when('I attempt to create an availibility', () => {
+		when('I attempt to create an availibility', async () => {
 			//Act
+			try {
+				// create a spy on the function that connects to the database
+				const createOneSpy = jest.spyOn(BaseService, 'createOne');
+				createOneSpy.mockReturnValue(
+					Promise.resolve({ start, end, days })
+				);
+
+				availability = await createAvailability({
+					start,
+					end,
+					days,
+				});
+			} catch (error: any) {
+				err = error;
+			}
 		});
 
 		then('I should get invalid details error', () => {
 			//Assert
+			expect(availability).toBeUndefined();
+			expect(err).toBeDefined();
+			expect(err.message).toEqual('Invalid details');
+		});
+
+		afterEach(() => {
+			// reset the mock after each test
+			jest.resetAllMocks();
 		});
 	});
 });
