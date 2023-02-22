2a67acf7585f5f417e3a1bc4f21286d616142ec7
YounesAEO <y.aboueleomoum09@gmail.com>
2023-02-22 21:19:34 +0100

    add fetchAll implementation to reservationService

--- src/modules/reservation/services/index.ts
+++ src/modules/reservation/services/index.ts
@@ -1,11 +1,25 @@
 import Joi, { Schema } from 'joi';
 import * as BaseService from './base';
 
+export const fetchAllReservations = (data: any) => {
+	// validate data
+	const schema: Schema = Joi.object({
+		query: Joi.object(),
+		sort: Joi.object(),
+	});
+	const { error } = schema.validate(data);
+	if (error) {
+		throw new Error('Bad request');
+	}
+	return BaseService.fetchAll({ query: data.query, sort: data.sort });
+};
+
 export const createReservation = (data: any) => {
 	// validate data
 	const schema: Schema = Joi.object({
 		start: Joi.date().required(),
 		end: Joi.date().required(),
+		name: Joi.string().required(),
 		email: Joi.string().email().required(),
 		title: Joi.string().required(),
 	});
