3608ac87aa1f0ebd024ad72a41b70dd43bf3e08b
YounesAEO <y.aboueleomoum09@gmail.com>
2023-02-22 21:18:13 +0100

    implement fetchAll, createMany to availability service

--- src/modules/availability/services/index.ts
+++ src/modules/availability/services/index.ts
@@ -1,12 +1,32 @@
 import Joi, { Schema } from 'joi';
 import * as BaseService from './base';
 
+export const fetchAllAvailabilities = (data: any) => {
+	// validate data
+	const schema: Schema = Joi.object({
+		query: Joi.object(),
+		sort: Joi.object(),
+	});
+	const { error } = schema.validate(data);
+	if (error) {
+		throw new Error('Bad request');
+	}
+
+	return BaseService.fetchAll({ query: data.query, sort: data.sort });
+};
+
 export const createAvailability = (data: any) => {
 	// validate data
 	const schema: Schema = Joi.object({
-		start: Joi.date().required(),
-		end: Joi.date().required(),
-		days: Joi.array().items(Joi.boolean()).length(7).required(),
+		start: Joi.object({
+			hours: Joi.number().required(),
+			minutes: Joi.number().required(),
+		}).required(),
+		end: Joi.object({
+			hours: Joi.number().required(),
+			minutes: Joi.number().required(),
+		}).required(),
+		days: Joi.array().items(Joi.number()).max(7).required(),
 	});
 	const { error } = schema.validate(data);
 	if (error) {
@@ -15,6 +35,45 @@ export const createAvailability = (data: any) => {
 
 	// create availability
 	return BaseService.createOne({ payload: data });
+};
+
+export const createAvailabilities = (data: any) => {
+	// validate data
+	const schema: Schema = Joi.object({
+		availabilities: Joi.array().items(
+			Joi.object({
+				start: Joi.object({
+					hours: Joi.number().required(),
+					minutes: Joi.number().required(),
+				}).required(),
+				end: Joi.object({
+					hours: Joi.number().required(),
+					minutes: Joi.number().required(),
+				}).required(),
+				days: Joi.array().items(Joi.number()).max(7).required(),
+			})
+		),
+		limit: Joi.array().items(Joi.date()).length(2),
+	});
+	const { error } = schema.validate(data);
+	if (error) {
+		console.log(error);
+		throw new Error('Invalid details');
+	}
+
+	const { availabilities, limit } = data;
+	const payload = limit
+		? availabilities.map((availability: any) => {
+				return {
+					...availability,
+					limitStart: limit[0],
+					limitEnd: limit[1],
+				};
+		  })
+		: availabilities;
+
+	// create availabilities
+	return BaseService.createMany({ payload });
 };
 
 export const deleteAvailability = async (data: any) => {
